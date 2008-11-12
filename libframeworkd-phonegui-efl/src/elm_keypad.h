065df01ab8685545f3dccae90856c2f66f22d8c5
Julien 'Ainulindale' Cassignol <ainulindale@gmail.com>
2008-11-12 11:31:51 +0100

    Prepared repository for separation.

--- libframeworkd-phonegui-efl/src/elm_keypad.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ELM_KEYPAD_H
-#define _ELM_KEYPAD_H
-
-#include <Elementary.h>
-
-EAPI Evas_Object* elm_keypad_add(Evas_Object *parent);
-
-#endif;
--- openmoko-messages3/autogen.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-REQUIRED_AUTOMAKE_VERSION=1.7 exec gnome-autogen.sh $@
